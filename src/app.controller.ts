1e22527b404956236379af8a706e81065693cf26
Gabriel Macêdo <gm8825123@gmail.com>
2023-12-01 16:27:55 -0300

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,6 +10,7 @@ export class AppController {
   // Get é um decorador que marca o método getHello como um manipulador de rota GET
   // para um caminho específico. Nesse caso Xablau.
   //  Isso significa que quando um cliente faz uma solicitação GET para o caminho "Xablau" no aplicativo, este método será chamado para lidar com a solicitação.
+ 
   @Get()
   getHello(): string {
     return this.appService.getHello();
